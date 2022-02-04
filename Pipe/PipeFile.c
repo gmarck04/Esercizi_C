5805148e29240e0c7bec403e09ab18cb20244be8
Giovanni <gmarck04@gmail.com>
2022-02-04 12:14:47 +0100

    Test

---
+++ Pipe/PipeFile.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main()
+{
+    int status;
+    int fd[2];
+    pipe(fd);
+    int pid1 = fork();
+    File *file;
+    file = fopen(out.txt, "w");
+    if (pid1 == 0) //Processo figlio 1
+    {
+        close(1);
+        dup(fd[1]);
+        close(fd[1]);
+        close(fd[0]);
+        execl("/bin/cat", "cat", "file.txt", NULL);
+        exit(1);
+    }
+    else
+    {
+        int pid2 = fork();
+        if (pid2 == 0) //Processo figlio 2
+        {
+            close(0);
+            dup(file);
+            close(fd[0]);
+            close(fd[1]);
+            char cmd[] = "wc -l";
+            execl("/bin/wc", cmd, NULL);
+            exit(2);
+        }
+        else
+        {
+            wait(&status);
+            close(fd[0]);
+            close(fd[1]);
+            printf("I processi figli hanno terminato\n");
+        }
+
+
+    }
+}
